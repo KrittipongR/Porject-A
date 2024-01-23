806208d7812246b03256a4203975f60140867978
Punnychai <rpunnachai@gmail.com>
2024-01-23 21:05:58 +0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import './DemoStyle.css';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('home');
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <button onClick={() => handlePageChange('home')}>Home</button>
+        <button onClick={() => handlePageChange('about')}>About</button>
+        <button onClick={() => handlePageChange('contact')}>Contact</button>
+      </nav>
+
+      <hr />
+
+      {currentPage === 'home' && (
+        <div style={{ textAlign: 'center', marginTop: '20vh' }}>
+          <h1 style={{ color: '#333', fontSize: '7em', fontWeight: '700' }}>GAYLORD WAS HERE</h1>
+        </div>
+      )}
+
+
+      {currentPage === 'about' && (
+        <div>
+          <h2>About Page</h2>
+          <p>This is the About page content.</p>
+        </div>
+      )}
+
+      {currentPage === 'contact' && (
+        <div>
+          <h2>Contact Page</h2>
+          <p>This is the Contact page content.</p>
+        </div>
+      )}
     </div>
   );
 }
