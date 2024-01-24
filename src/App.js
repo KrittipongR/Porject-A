d55cdae428db726b0cf58024a8eb438430691e0b
Punnychai <rpunnachai@gmail.com>
2024-01-24 21:52:40 +0700

    Create landing page

--- src/App.js
+++ src/App.js
@@ -3,43 +3,68 @@ import './App.css';
 import './DemoStyle.css';
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('home');
+  const [currentPage, setCurrentPage] = useState('land');
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
   };
 
-  return (
-    <div>
+  const NavBar = () => {
+    return (
       <nav>
         <button onClick={() => handlePageChange('home')}>Home</button>
         <button onClick={() => handlePageChange('about')}>About</button>
         <button onClick={() => handlePageChange('contact')}>Contact</button>
       </nav>
+    )
+  };
 
-      <hr />
+  return (
+    <>
+
+      {currentPage === 'land' && (
+        <div style={{ textAlign: 'center', marginTop: '20vh' }} onClick={() => handlePageChange('home')}>
+          <div style={{ height: '10vw', width: '10vw', backgroundColor: "green", marginLeft: "45vw"}}>
+            <p style={{ color: "white" }}>Logo Here</p>
+          </div>
+          <h1 style={{ color: '#333', fontSize: '4em', fontWeight: '700' }}>
+            <span style={{color: '#5CE16E' }}>Scrapy's</span> Commission
+          </h1>
+        </div>
+      )}
 
       {currentPage === 'home' && (
-        <div style={{ textAlign: 'center', marginTop: '20vh' }}>
-          <h1 style={{ color: '#333', fontSize: '7em', fontWeight: '700' }}>GAYLORD WAS HERE</h1>
-        </div>
+        <>
+          <NavBar />
+          <div>
+            <h2>Home Page</h2>
+            <p>This is the Home page content.</p>
+          </div>
+        </>
       )}
 
 
       {currentPage === 'about' && (
-        <div>
-          <h2>About Page</h2>
-          <p>This is the About page content.</p>
-        </div>
+        <>
+          <NavBar />
+          <div>
+            <h2>About Page</h2>
+            <p>This is the About page content.</p>
+          </div>
+        </>
       )}
 
       {currentPage === 'contact' && (
-        <div>
-          <h2>Contact Page</h2>
-          <p>This is the Contact page content.</p>
-        </div>
+        <>
+          <NavBar />
+          <div>
+            <h2>Contact Page</h2>
+            <p>This is the Contact page content.</p>
+          </div>
+        </>
       )}
-    </div>
+    
+    </>
   );
 }
 
